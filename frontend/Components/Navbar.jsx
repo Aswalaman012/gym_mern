34c0ec52cb3c536ed343d390187dd4e47a731b65
Aswalaman012 <112434929+Aswalaman012@users.noreply.github.com>
2024-06-12 16:09:51 +0530

    first

---
+++ frontend/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <header className='Navbar'>
+        <img src='png.png'></img>
+        <p> FITNESS JUNGLE</p>
+    </header>
+   
+    </>
+  )
+}
+
+export default Navbar
